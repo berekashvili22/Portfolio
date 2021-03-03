f1a14849ac43f2d6f77a4af3b188db54a50b6284
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-03 20:32:29 +0400

    add existing project

---
+++ mysite/portfolio/static/portfolio/loader.js
@@ -0,0 +1,18 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+        setTimeout(function() {    
+            document.getElementById("loadCont").className = "hidden";
+        }, 5300);
+    },
+    false
+    );
+
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+        setTimeout(function() {    document.getElementById("loaderLogo").className = "hidden";
+        }, 5000);
+    },
+    false
+    );
