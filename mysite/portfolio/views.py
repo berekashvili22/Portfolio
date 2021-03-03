42598bb4dd92a996775b63fb3e6c32aea312ff5f
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-03 21:09:49 +0400

    add resume view

--- mysite/portfolio/views.py
+++ mysite/portfolio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'portfolio/index.html')
+
+def resume(request):
+    return render(request, 'portfolio/resume.html')
